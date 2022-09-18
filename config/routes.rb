9a964c03c8661abede8d3c03cf525b543c1af6ab
Sang Huynh <sang.huynh@innogram.co>
2022-09-18 07:59:03 +0700

    [FIN-3] Change path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :folders
-  resources :resources, only: %i(create destroy)
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :items
+      resources :folders
+      resources :resources, only: %i(create destroy)
+    end
+  end
 end
